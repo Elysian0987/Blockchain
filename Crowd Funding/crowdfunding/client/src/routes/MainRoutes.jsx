a85e493b21ffb0f71f3f8fdda0b3c45861c951bd
Samrudhi Bhat <samrudhibhat29@gmail.com>
2024-06-15 16:07:32 +0530

    split1

---
+++ Crowd Funding/crowdfunding/client/src/routes/MainRoutes.jsx
@@ -0,0 +1,53 @@
+import { lazy } from 'react';
+
+// project imports
+import MainLayout from 'layout/MainLayout';
+import Loadable from 'ui-component/Loadable';
+
+// dashboard routing
+const DashboardDefault = Loadable(lazy(() => import('views/dashboard')));
+
+// page routing
+
+const Home = Loadable(lazy(() => import('views/home')));
+
+const Campaign = Loadable(lazy(() => import('views/create-campaigns')));
+
+const CampaignDetails = Loadable(lazy(() => import('views/campaign-details')));
+
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  element: <MainLayout />,
+  children: [
+    {
+      path: '/',
+      element: <DashboardDefault />
+    },
+    {
+      path: 'dashboard',
+      children: [
+        {
+          path: 'default',
+          element: <DashboardDefault />
+        }
+      ]
+    },
+    {
+      path: '/home',
+      element: <Home />
+    },
+    {
+      path: '/create-campaigns',
+      element: <Campaign />
+    },
+    {
+      path: '/campaign-details/:title',
+      element: <CampaignDetails />
+    }
+  ]
+};
+
+export default MainRoutes;
--- crowdfunding/client/src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const crowdFundingContractAddress = import.meta.env.VITE_CONTRACT_ADDRESS;
-
-export const thirdWebClientId = import.meta.env.VITE_THIRDWED_CLIENT_ID;
-
